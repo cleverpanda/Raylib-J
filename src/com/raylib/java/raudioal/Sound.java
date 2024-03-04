1f69ab787928aaa8984367edbd75874f58d138d9
Cleverpanda <byrce6@gmail.com>
2024-03-04 16:57:14 -0600

    Migrate to Gradle

--- src/com/raylib/java/raudioal/Sound.java
+++
@@ -1,9 +0,0 @@
-package com.raylib.java.raudioal;
-
-public class Sound {
-
-    int source;    // OpenAL audio source id
-    int buffer;    // OpenAL audio buffer id
-    int format;             // OpenAL audio format specifier
-
-}
